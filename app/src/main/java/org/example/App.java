47c24948afcb78122bef3c8b4341e857f4985ddc
NNR8608220 <n8reyes2006@gmail.com>
2025-01-21 19:43:56 -0600

    Finally Finished

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,5 +3,7 @@ package org.example;
 public class App {
   public static void main(String[] args) {
     // Your code goes here
+    HelloWorld hello = new HelloWorld();
+    System.out.println(hello.getGreeting());
   }
 }
--- app/src/main/java/org/example/HelloWorld.java
+++ app/src/main/java/org/example/HelloWorld.java
@@ -2,4 +2,8 @@ package org.example;
 
 public class HelloWorld {
   // Your code goes here
+  String getGreeting()
+  {
+    return "Hello World!";
+  }
 }
